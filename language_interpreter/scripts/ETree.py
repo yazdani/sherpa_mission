a69b45ae4129475fcbdf1fbf45bbe77352b6c366
Yazdani <yazdani@cs.uni-bremen.de>
2015-04-01 17:54:01 +0200

    added the modified node for the integration with WP4

--- language_interpreter/scripts/ETree.py
+++ language_interpreter/scripts/ETree.py
@@ -3,11 +3,13 @@
 
 __author__= 'Fereshta Yazdani'
 
+import socket, pickle
 import rospy
 import roslib
 import xml.etree.ElementTree as ET 
 from language_interpreter.msg import StringMultiArrays
 from language_interpreter.msg import StringMultiArray
+from geometry_msgs.msg import Vector3
 import tf
 import os
 import geometry_msgs.msg
@@ -15,19 +17,24 @@ import std_msgs.msg
 import language_interpreter.msg
 
 array = []
+var = ''
+iterator = []
 
 def main():
-   
     global pub1
+    # sock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    # server_address = ("yazdani",5678)
+    # sock.connect(server_address)
     rospy.init_node('HRI_command')
-    pub1 = rospy.Publisher('intern_call',language_interpreter.msg.StringMultiArrays , queue_size=10)
+    rate = rospy.Rate(0.1) 
+    pub1 = rospy.Publisher('internal_call',language_interpreter.msg.StringMultiArrays , queue_size=10)
     rospy.Subscriber("HRIOut", std_msgs.msg.String, callback)
     rospy.spin()
 
 def callback(data):
     ar2 = StringMultiArrays()
     ar1 = StringMultiArray()
-    print type(ar2)
+    vec = Vector3()
     array = []
     #Reading the data from a string
     root = ET.fromstring(data.data)
@@ -39,15 +46,32 @@ def callback(data):
                 array.append([child4[1], child2.text])
    
     if len(array) == 4:
-        array.append(["blank", "blank"])
+        array.append(["command", "red hawk"])
+
+    for pointer in array:
+        if pointer[0] == "command":
+            var = pointer[1]
+            print var
+        else if pointer[0] == "gesture":
+            print "you have to split first"
+            vec_array = pointer[1].split(" ")
+            vec.x = float(vec_array[0])
+            vec.y = float(vec_array[1])
+            vec.z = float(vec_array[2])
+            
     ar1.str1 = array[0]
     ar1.str2 = array[1]
     ar1.str3 = array[2]
     ar1.str4 = array[3]
     ar1.str5 = array[4]
     ar2.multi.append(ar1)
-  
+    # print array[2][1]
+    # iterator = ([var,vec])
+    # data = pickle.dumps(iterator)
+    # sock.recv(1024)
+    # sock.send(data)
     pub1.publish(ar2)
+    #iterator = ([])
   
 if __name__ == '__main__':
     try:
--- language_interpreter/scripts/connector.py
+++ language_interpreter/scripts/connector.py
@@ -2,9 +2,7 @@
 
 import socket, pickle
 import rospy
-from language_interpreter.msg import testmsg
-from language_interpreter.msg import checkmsg
-from language_interpreter.msg import tldl2Ros
+from language_interpreter.msg import connector
 import sys
 import std_msgs.msg 
 import geometry_msgs.msg
@@ -34,18 +32,18 @@ def check_com(date):
     global str1
     str1 = date 
     data = str1.split(" ")
-    a = ''.join(['A', 'r', 'e'])
-    b = ''.join(['D', 'o'])
-    c = ''.join(['D', 'o', 'e', 's'])
-    d = ''.join(['H', 'a', 'v', 'e'])
-    e = ''.join(['H', 'a', 's'])
-    f = ''.join(['H', 'o', 'w'])
-    g = ''.join(['W', 'h', 'a', 't'])
-    h = ''.join(['W', 'h', 'i', 'c', 'h'])
-    i = ''.join(['W', 'h', 'e', 'r', 'e'])
-    j = ''.join(['W', 'h', 'o'])
-    k = ''.join(['W', 'h', 'o', 's', 'e'])
-    l = ''.join(['W', 'h', 'y'])
+    a = ''.join(['a', 'r', 'e'])
+    b = ''.join(['d', 'o'])
+    c = ''.join(['d', 'o', 'e', 's'])
+    d = ''.join(['h', 'a', 'v', 'e'])
+    e = ''.join(['h', 'a', 's'])
+    f = ''.join(['h', 'o', 'w'])
+    g = ''.join(['w', 'h', 'a', 't'])
+    h = ''.join(['w', 'h', 'i', 'c', 'h'])
+    i = ''.join(['w', 'h', 'e', 'r', 'e'])
+    j = ''.join(['w', 'h', 'o'])
+    k = ''.join(['w', 'h', 'o', 's', 'e'])
+    l = ''.join(['w', 'h', 'y'])
     
     
     if data[0] == a or data[0] == b or data[0] == c or data[0] == d or data[0] == e or data[0] == f or data[0] == g or data[0] == h or data[0] == i or data[0] == j or data[0] == k or data[0] == l:
@@ -55,18 +53,18 @@ def check_com(date):
         
 def callback():
     socket_conTest()
-    pub.publish(str1, str2, tr1, tr2)
+    pub.publish(str1, str2, tr1)
     
     
 def socket_conTest():
     global tr1
-    global tr2
+    #global tr2
+    #global tr3
     c_test.send("ok\n")
     sys.stdout.flush()
     data = c_test.recv(1024)
     date = pickle.loads(data)
     tr1 = date[1]
-    tr2 = date[2]
     socket_con(date[0])    
     check_com(date[0])
     
@@ -77,7 +75,7 @@ def connector():
     global c_test
     global pub
     rospy.init_node('tf_transforms')
-    pub = rospy.Publisher('interpreted_command',tldl2Ros, queue_size=10)
+    pub = rospy.Publisher('interpreted_command',connector, queue_size=10)
     s_test = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     s_test.bind(("yazdani", 5678))
     s_test.listen(1)
