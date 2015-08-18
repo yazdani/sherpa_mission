6ba5f5267aec9a8860a9ee8bba62a6244ab09a8e
Fereshta Yazdani <yazdani@cs.uni-bremen.de>
2015-08-18 18:32:55 +0200

    added new files for the new grammar of the diarc architecture

--- multimodal_interpreter/src/interpreter.py
+++ multimodal_interpreter/src/interpreter.py
@@ -3,36 +3,98 @@
 import socket, pickle
 import rospy
 from std_msgs.msg import String
+from multimodal_interpreter.msg import multimodal_msgs
+from multimodal_interpreter.msg import response_msgs
 import sys
 
 pub=''
 str2=''
 str1=''
+sel=''
+dat=''
+typ=''
+dire=''
+loc=''
+poly=''
+seg=''
+circ=''
+pose=''
+
+
 def callback(date):
-     print "moin"
-     global str2
-     tmp = date.data + '\n'
-     print tmp
+     declareVariables(date)
+     tmp = date.command
+     rplace = tmp.replace("_", " ")
+     checkCommandType(rplace)
+     sendSocket(rplace)
+
+
+def declareVariables(date):
+     global sel
+     global dat
+     global dire
+     global loc
+     global poly
+     global seg
+     global circ
+     global pose
+     sel = date.selected
+     dat = date.data
+     dire = date.direction
+     loc = date.location
+     poly = date.polygonal_area
+     seg = date.segment
+     circ = date.circ_area
+     pose = date.sample
+
+
+def checkCommandType(date):
+     global typ
+     typ = date 
+     data = typ.split(" ")
+     a = ''.join(['a', 'r', 'e'])
+     b = ''.join(['d', 'o'])
+     c = ''.join(['d', 'o', 'e', 's'])
+     d = ''.join(['h', 'a', 'v', 'e'])
+     e = ''.join(['h', 'a', 's'])
+     f = ''.join(['h', 'o', 'w'])
+     g = ''.join(['w', 'h', 'a', 't'])
+     h = ''.join(['w', 'h', 'i', 'c', 'h'])
+     i = ''.join(['w', 'h', 'e', 'r', 'e'])
+     j = ''.join(['w', 'h', 'o'])
+     k = ''.join(['w', 'h', 'o', 's', 'e'])
+     l = ''.join(['w', 'h', 'y'])
      
+     
+     if data[0] == a or data[0] == b or data[0] == c or data[0] == d or data[0] == e or data[0] == f or data[0] == g or data[0] == h or data[0] == i or data[0] == j or data[0] == k or data[0] == l:
+          typ = "ask"
+     else:
+          typ = "order"
+          
+
+def sendSocket(date):
+     global str2
+     tmp = date + '\n'
      c.send(tmp)
      sys.stdout.flush()
      str2 = c.recv(1024)
-     pub.publish(str2)
+     pub.publish(sel, str2, typ, dat, dire, loc, poly, seg, circ, pose)
 
-def interpret():
+
+def startSocket():
     global pub
     global c
     rospy.init_node('tf_transforms2')
-    pub = rospy.Publisher('multimodal_interpreter', String, queue_size=10)
+    pub = rospy.Publisher('multimodal_interpreter', response_msgs , queue_size=10)
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind(("yazdani", 1234))
     s.listen(1)
     server = "yazdani"
     c, address = s.accept()
-    rospy.Subscriber("test_msgs", String, callback)
+    rospy.Subscriber("test_msgs", multimodal_msgs, callback)
     rospy.spin()
     s.shutdown(socket.SHUT_RDWR)
     s.close()
 
 if __name__ == '__main__':
-    interpret()
+    startSocket()
--- multimodal_interpreter/src/tester.py
+++ multimodal_interpreter/src/tester.py
@@ -1,14 +1,79 @@
 #!/usr/bin/env python
 
 import rospy
+from multimodal_interpreter.msg import multimodal_msgs
 from std_msgs.msg import String
+from geometry_msgs.msg import Vector3
+from geometry_msgs.msg import Pose
+
 
 if __name__ == '__main__':
-   
-    pub = rospy.Publisher('test_msgs', String, queue_size=10)
+    pub = rospy.Publisher('test_msgs', multimodal_msgs, queue_size=10)
     rospy.init_node('tf_transforms')
-    rate = rospy.Rate(40) 
+    rate = rospy.Rate(0.1) 
+    circ = Vector3()
+    str1 = multimodal_msgs()
+    pose = Pose()
     while not rospy.is_shutdown():
-        hello_str = "Go right of this tree"
-        pub.publish(hello_str)
+        str1.selected = "red hawk"
+        str1.type = "go"
+        str1.command = "Go right of this tree"
+        str1.data = 1.0
+        str1.direction = [2.0, 0.0, 0.0]
+        str1.location = [3.0, 0.0, 0.0]
+        str1.confidence = 1.98
+        str1.polygonal_area = []
+        str1.segment = []
+        str1.circ_area.x = 0.0 
+        str1.circ_area.y = 0.0
+        str1.circ_area.z = 0.0
+        str1.radius = 0.0
+        str1.source = "gesture"
+        str1.sample.position.x = 0.0
+        str1.sample.position.y  = 0.0
+        str1.sample.position.z = 0.0
+        str1.sample.orientation.x = 0.0       
+        str1.sample.orientation.y = 0.0        
+        str1.sample.orientation.z = 0.0
+        str1.sample.orientation.w = 0.0
+        pub.publish(str1)
         rate.sleep()
+       # hello_str = "Have you seen any victims"
+       # pub.publish(hello_str)
+       # rate.sleep()
+       # hello_str = "How are the weather conditions there"
+       # pub.publish(hello_str)
+       # rate.sleep()
+       # hello_str = "Take a picture"
+       # pub.publish(hello_str)
+      #  rate.sleep()
+       # hello_str = "Take a video"
+      #  pub.publish(hello_str)
+      #  rate.sleep()
+       # hello_str = "Where is the victim located"
+        #pub.publish(hello_str)
+        #rate.sleep()
+       # hello_str = "Report me the location"
+       # pub.publish(hello_str)
+       # rate.sleep()
+       # hello_str = "How are the conditions around the survivor"
+       # pub.publish(hello_str)
+       # rate.sleep()
+       # hello_str = "Go further"
+       # pub.publish(hello_str)
+       # rate.sleep()
+       # hello_str = "Look for some victims"
+       # pub.publish(hello_str)
+       # rate.sleep()
+       # hello_str = "Tell me the conditions"
+       # pub.publish(hello_str)
+       # rate.sleep()
+       # hello_str = "Search for a cap"
+       # pub.publish(hello_str)
+       # rate.sleep()
+       # hello_str = "What do you see"
+       # pub.publish(hello_str)
+       # rate.sleep()
+       # hello_str = "Search for a yellow cap"
+       # pub.publish(hello_str)
+       # rate.sleep()
